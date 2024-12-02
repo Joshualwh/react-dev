c08ff5c8aa67af5ba1ccfb07717e3ded0add1022
Joshualwh <joshualing0711@gmail.com>
2024-12-03 07:27:59 +0800

    Worked on a few css components.

--- crwn-clothing/src/components/sign-in-form/sign-in-form.component.jsx
+++ crwn-clothing/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import {
   signInWithGooglePopup,
   signInAuthUserWithEmailAndPassword,
@@ -32,7 +32,7 @@ const SignInForm = () => {
       await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch (error) {
-      switch(error.code) {
+      switch (error.code) {
         case 'auth/wrong-password':
           alert('incorrect password for email');
           break;
@@ -54,12 +54,12 @@ const SignInForm = () => {
     <div className="sign-up-container">
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={handleSubmit}>
         <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email} />
         <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+          <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google Sign In</Button>
         </div>
       </form>
     </div>
